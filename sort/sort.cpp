312f50f8bc01697392a444e95e945c001a36224e
7mantel7 <aper29012006@gmail.com>
2023-12-19 20:52:27 +0300

    l

---
+++ sort/sort.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    int list[4] = { 12, 4, 7, 1 };
+
+    for (int i : list) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    for (int i = 1; i < 4; i++) {
+        int j = i - 1;
+        while (j >= 0 && list[j] > list[j + 1]) {
+            swap(list[j], list[j + 1]);
+            j--;
+        }
+    }
+
+    for (int i : list) {
+        cout << i << " ";
+    }
+    cout << endl;
+}
